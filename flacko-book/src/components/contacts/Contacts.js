2bf17d6d0a60cd739d9dc1a6f4804985ef590bd8
rjhelm <rjhelm@github.io>
2022-05-23 15:06:41 -0600

    created contacts component

---
+++ flacko-book/src/components/contacts/Contacts.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { Avatar, Tooltip, Paper, Divider } from '@mui/material';
+import { Scrollbars } from 'react-custom-scrollbars';
+import SearchIcon from '@mui/icons/Search';
+import MoreHorizon from '@mui/icons/MoreHorizon';
+import InfoBar from '../util/InfoBar';
+import Style from './Style';
+import axios from 'axios';
+
+const Contacts = () => {
+    const classes = Style();
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const response = await axios.get("https://breakingbadapi.com/api/characters");
+            setUsers(response.data);
+        };
+        fetchUsers();
+    }, []);
+
+    return (
+        <Paper elevation={0} className={classes.contacts}>
+            <Scrollbars autoHide autoHideDuration={200}>
+                <Divider />
+                <div className={classes.contacts__tab}>
+                    <h4>Your Pages</h4>
+                    <MoreHorizon />
+                </div>
+                <Divider />
+                <div className={classes.contacts__tab}>
+                    <h4>Contacts</h4>
+                    <SearchIcon />
+                    <MoreHorizon />
+                </div>
+                {users.map(({ char_id, name, img }) => (
+                    <InfoBar
+                        key={char_id}
+                        Source={
+                            <Tooltip placement="left" title={name} arrow>
+                                <Avatar src={img} size={100} />
+                            </Tooltip>
+                        }
+                        title={name}
+                        online={true}
+                        lastSeen={
+                            Math.floor(Math.random() * (3 - 1 + 1)) + 1 === 2 && `${Math.floor(Math.random() * 10) + 1} h`
+                        }
+                        noTransform={true}
+                    />
+                ))}
+            </Scrollbars>
+        </Paper>
+    );
+};
+
+export default Contacts;
